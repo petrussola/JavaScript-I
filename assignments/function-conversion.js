9bc0edd8604a1c7cd93eaf5c5af7102e1da002b9
Pere <pere.solaclaver@gmail.com>
2019-08-19 13:51:38 +0100

    Solve challenge 3 arrow functions

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -18,7 +18,9 @@ anotherFunction("Example");
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
-// add(1,2);
+
+let add = (param1, param2) => param1 + param2;
+add(1,2);
 
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
