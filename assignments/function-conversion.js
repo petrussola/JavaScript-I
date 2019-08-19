bdfcac44669540be113aa5cbaa7ba70d64b1563f
Pere <pere.solaclaver@gmail.com>
2019-08-19 13:48:23 +0100

    Solve challenge 2 arrow functions

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -11,7 +11,9 @@ myFunction();
 //   return param;
 // };
 
-// anotherFunction("Example");
+let anotherFunction = (param) => param;
+
+anotherFunction("Example");
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
